c8494824caa6a8bff70385b88cf85625f90d5b81
haoyuant <haoyuant94718@gmail.com>
2019-04-09 13:10:47 +1000

    first commit

---
+++ app.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+var mapRouter = require('./routes/map');
+var osmRouter = require('./routes/osm');
+
+var app = express();
+app.set('view engine', 'ejs');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+app.use('/map', mapRouter);
+app.use('/osm', osmRouter);
+
+module.exports = app;
